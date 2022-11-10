c9d31ecde9094c47ee9dcb86b4afb85f5ce61ff7
SeanBristol-Lee <redgreyblu@sbcglobal.net>
2022-11-10 03:12:44 -0800

    added two prompts

--- Develop/index.js
+++ Develop/index.js
@@ -3,9 +3,45 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = ['description', 'contents', 'installation', 'usage', 'license', 'contributing', 'tests', 'question'];
+//const questions = ['description', 'contents', 'installation', 'usage', 'license', 'contributing', 'tests', 'question'];
 
-const [description, contents, installation, usage, license, contributing, tests, question] = questions;
+//const [description, contents, installation, usage, license, contributing, tests, question] = questions;
+
+const generateReadMe = ({ title, description, tableOfContents, installation, usage, license, contributing, tests, questions}) =>
+`
+# ${title} 
+
+## ${description}
+
+## ${tableOfContents}
+
+## ${installation}
+
+## ${usage}
+
+## ${license}
+
+## ${contributing}
+
+## ${tests}
+
+## ${questions}
+`;
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            title: 'title',
+            message: 'Type the title of your project',
+        },
+        {
+            type: 'input',
+            description: 'description',
+            message: 'What was your motivation for this project?'
+        }
+    ])
+    
 
 // TODO: Create a function to write README file
 fs.writeToFile('README.md', readmeContent, (err) => err ? console.log(err) : console.log("README.md created!"));
