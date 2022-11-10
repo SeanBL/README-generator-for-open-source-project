07f5c56590ff154a809438dbe1cab0b21515a8ee
SeanBristol-Lee <redgreyblu@sbcglobal.net>
2022-11-10 00:39:44 -0800

    added to array and included writeFile function

--- Develop/index.js
+++ Develop/index.js
@@ -3,10 +3,12 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['description', 'contents', 'installation', 'usage', 'license', 'contributing', 'tests', 'question'];
+
+const [description, contents, installation, usage, license, contributing, tests, question] = questions;
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+fs.writeToFile('README.md', readmeContent, (err) => err ? console.log(err) : console.log("README.md created!"));
 
 // TODO: Create a function to initialize app
 function init() {}
